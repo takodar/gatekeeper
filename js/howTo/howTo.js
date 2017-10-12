1adba6b10d81d5ed3906c90bbca09ab39e183e0a
Takoda Register <Takodr2@gmail.com>
2017-10-11 20:31:07 -0400

    more work on how to page

--- js/howTo/howTo.js
+++ js/howTo/howTo.js
@@ -1,37 +1,56 @@
 var howToList = $("#howtoList");
 var howToWelcomeMessage = $("#howToWelcomeMessage");
 var howToDocuments = $("#howtoIframe");
+var howToFAQs = $("#howToFAQs");
 var navHowToHome = $("#navHowToHome");
 var navHowToList = $("#navHowToList");
 var navDocuments = $("#navDocuments");
+var navFAQs = $("#navFAQs");
 var btnHelp = $("#btnHelp");
 
-function alterDisplay(displayDiv){
-    if(displayDiv === 'howtoList'){
-	howToList.css("display", "inline");
-	howToWelcomeMessage.css("display", "none");
-	howToDocuments.css("display", "none");
-	navHowToList.addClass("active");
-	navHowToHome.removeClass("active");
-	navDocuments.removeClass("active");
-	btnHelp.data('help', 'howTo');
+function alterDisplay(displayDiv) {
+    if (displayDiv === 'howtoList') {
+        howToList.css("display", "inline");
+        howToWelcomeMessage.css("display", "none");
+        howToDocuments.css("display", "none");
+        howToFAQs.css("display", "none");
+        navHowToList.addClass("active");
+        navHowToHome.removeClass("active");
+        navDocuments.removeClass("active");
+        navFAQs.removeClass("active");
+        btnHelp.data('help', 'howTo');
     }
-    else if(displayDiv === 'howToWelcomeMessage'){
-	howToWelcomeMessage.css("display", "inline");
-	howToList.css("display", "none");
-	howToDocuments.css("display", "none");
-	navHowToHome.addClass("active");
-	navHowToList.removeClass("active");
-	navDocuments.removeClass("active");
-	btnHelp.data('help', 'home');
+    else if (displayDiv === 'howToWelcomeMessage') {
+        howToWelcomeMessage.css("display", "inline");
+        howToList.css("display", "none");
+        howToDocuments.css("display", "none");
+        howToFAQs.css("display", "none");
+        navHowToHome.addClass("active");
+        navHowToList.removeClass("active");
+        navDocuments.removeClass("active");
+        navFAQs.removeClass("active");
+        btnHelp.data('help', 'home');
     }
-    else if(displayDiv === 'howtoIframe'){
-	howToDocuments.css("display", "inline");
-	howToList.css("display", "none");
-	howToWelcomeMessage.css("display", "none");
-	navDocuments.addClass("active");
-	navHowToList.removeClass("active");
-	navHowToHome.removeClass("active");
-	btnHelp.data('help', 'documents');
+    else if (displayDiv === 'howtoIframe') {
+        howToDocuments.css("display", "inline");
+        howToList.css("display", "none");
+        howToWelcomeMessage.css("display", "none");
+        howToFAQs.css("display", "none");
+        navDocuments.addClass("active");
+        navHowToList.removeClass("active");
+        navHowToHome.removeClass("active");
+        navFAQs.removeClass("active");
+        btnHelp.data('help', 'documents');
+    }
+    else if (displayDiv === 'howToFAQs') {
+        howToFAQs.css("display", "inline");
+        howToDocuments.css("display", "none");
+        howToList.css("display", "none");
+        howToWelcomeMessage.css("display", "none");
+        navFAQs.addClass("active");
+        navDocuments.removeClass("active");
+        navHowToList.removeClass("active");
+        navHowToHome.removeClass("active");
+        btnHelp.data('help', 'documents');
     }
 }
